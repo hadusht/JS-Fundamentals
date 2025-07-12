e75e26dc61c2a1fefb23eb2178696b88a6843dc1
Hadush Tekulu <hadetek@gmail.com>
2025-07-12 18:32:34 +0300

    3rd commit

--- 9-add.js
+++ 9-add.js
@@ -1,16 +1,12 @@
-const add = (a, b) => {
-  const num1 = parseInt(a, 10);
-  const num2 = parseInt(b, 10);
-  
-  if (isNaN(num1) || isNaN(num2)) {
-    console.log('Arguments must be integers');
-  } else {
-    console.log(num1 + num2);
-  }
-};
+function add(a, b) {
+      return a + b;
+    }
 
-// Get command line arguments (skip node and script path)
-const [,, arg1, arg2] = process.argv;
+    const button = document.getElementById('addButton');
 
-// Call the function with the provided arguments
-add(arg1, arg2);
+    button.addEventListener('click', () => {
+      const a = parseInt(document.getElementById('num1').value, 10);
+      const b = parseInt(document.getElementById('num2').value, 10);
+
+      console.log(add(a, b));
+    });
