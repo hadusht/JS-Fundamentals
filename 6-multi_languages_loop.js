10959bdb8c33cc5ae0107ac6446ec0f5851cb4b8
Hadush Tekulu <hadetek@gmail.com>
2025-07-12 17:22:20 +0300

    2nd commit

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const lang = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let lang = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for(let i = 0; i < lang.length; i++) {
     console.log(lang[i]);
 }
