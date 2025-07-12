dc506a9e06a03a51090491bffe893308468ba106
Hadush Tekulu <hadetek@gmail.com>
2025-07-12 18:26:44 +0300

    2nd commit for 9-add file

--- 9-add.js
+++ 9-add.js
@@ -1,7 +1,16 @@
-function add(a, b) {
-    a = 10;
-    b = 5;
-    console.log(a + b);
+const add = (a, b) => {
+  const num1 = parseInt(a, 10);
+  const num2 = parseInt(b, 10);
+  
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log('Arguments must be integers');
+  } else {
+    console.log(num1 + num2);
+  }
+};
 
-}
-add()
+// Get command line arguments (skip node and script path)
+const [,, arg1, arg2] = process.argv;
+
+// Call the function with the provided arguments
+add(arg1, arg2);
