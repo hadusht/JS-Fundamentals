7a79951e6478f58196ea40ec7247b9b4d49b3820
Hadush Tekulu <hadetek@gmail.com>
2025-07-12 18:42:07 +0300

    6th commit

--- 9-add.js
+++ 9-add.js
@@ -3,12 +3,15 @@ const add = (a, b) => {
   const num2 = parseInt(b, 10);
   
   if (isNaN(num1) || isNaN(num2)) {
-    console.log('Arguments must be integers');
+    console.log('NaN');
   } else {
     console.log(num1 + num2);
   }
 };
 
-const [,, arg1, arg2] = process.argv;
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
 
 add(arg1, arg2);
